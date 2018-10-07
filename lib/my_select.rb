4a3aa3cb2a556644f1934c5858055bb48cd115e0
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 18:13:42 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,14 +2,13 @@ def my_select(collection)
   i = 0 
   na = []
   
-  while i < collection.length 
-    na << yield(collection[i])
-    i += 1
-  end
-  
   if collection.include?(false)
     nil 
   else 
-    na
+    while i < collection.length 
+    na << yield(collection[i])
+    i += 1
+  end
+  na
   end
 end
